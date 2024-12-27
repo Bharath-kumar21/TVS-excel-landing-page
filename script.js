68603dc35e3d86e42a62c0dc11962a259a703a58
BHARATH KUMAR TALAGANA <113962239+Bharath-kumar21@users.noreply.github.com>
2024-12-28 00:06:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add simple form handling
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting us. We will get back to you soon!');
+});
